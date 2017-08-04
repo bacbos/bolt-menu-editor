5c5c0f79fc5584393813380f1e3653ace229779d
Santino Petrovic <santinopetrovic@gmail.com>
2017-08-04 09:00:03 +0200

    Added event which add additional fields to menueditor.

---
+++ src/Event/FieldBuilderEvent.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bolt\Extension\Bacboslab\Menueditor\Event;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\ParameterBag;
+/**
+ * Event fired prior to menueditor's additional fields construction.
+ *
+ * @author Santino Petrovic <santinopetrovic@gmail.com>
+ */
+class FieldBuilderEvent extends Event
+{
+    const BUILD = 'menueditor.fields.construction';
+    /** @var array */
+    protected $fields;
+
+    /**
+     * @return array
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+    public function setFields($fields)
+    {
+        $this->fields[] = $fields;
+    }    
+}
